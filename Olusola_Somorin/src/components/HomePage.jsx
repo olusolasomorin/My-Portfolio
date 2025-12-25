a23293d330b4baf12927a87b9abe2c7eaf870d24
olusola <somorinolusola1@gmail.com>
2025-12-25 16:12:43 +0100

    cv added

--- Olusola_Somorin/src/components/HomePage.jsx
+++ Olusola_Somorin/src/components/HomePage.jsx
@@ -1,4 +1,5 @@
 import dp2 from '../assets/dp3.PNG';
+
 import { Mail } from 'lucide-react';
 import { Download } from 'lucide-react';
 import { Linkedin } from 'lucide-react';
@@ -35,6 +36,8 @@ function HomePage() {
         }
     ]
 
+    // const cv = "/Olusola_CV.pdf"
+
     return (
         <div>
             <div className='hidden md:flex max-w-7xl mx-auto justify-between items-center px-4 my-25'>
@@ -57,8 +60,16 @@ function HomePage() {
                     </p>
 
                     <button className='flex gap-2 justify-center bg-cyan-400 py-5 text-xl font-semibold rounded-xl mx-5 mt-10 hover:bg-cyan-300 cursor-pointer hover:scale-105 transition-all duration-1000'>
-                        <span> <Download/> </span>
-                        Download Resume
+                        <a 
+                            href="/Olusola_CV.pdf"
+                            download="Olusola_Somorin_CV.pdf"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className='flex gap-2'
+                        >
+                            <Download/>
+                            Download Resume
+                        </a>
                     </button>
 
                     <button className='text-cyan-400 py-5 mx-5 mt-4 text-xl font-semibold border-2 border-cyan-400 rounded-xl hover:bg-cyan-300 hover:text-black cursor-pointer hover:border-0 hover:scale-105 transition-all duration-1000'>
